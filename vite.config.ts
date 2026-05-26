9af96dcc7bf84ceee9e6cea60946db51590dc0ce
suizyu <suizyu_m@yahoo.co.jp>
2026-05-26 09:23:37 +0900

    vite.config.ts の更新
    公開用のbase指定を追加

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/mock-web-audio/'
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
